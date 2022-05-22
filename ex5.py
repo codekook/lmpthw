aded0c6f3207c3897f1e08feab14ad7271bc0139
codekook <ralph.corey.1@gmail.com>
2022-05-22 16:41:52 -0700

    lmpthw ex 5: Refactored ex5 adding argparse

--- ex5.py
+++ ex5.py
@@ -1,4 +1,4 @@
-import sys
+import sys, argparse
 
 def doc_reader():
     #Check for two arguments
@@ -19,4 +19,35 @@ def doc_reader():
     doc.closed
     newfile.close()
 
-doc_reader()
+#doc_reader()
+
+'''Refactored with argparse'''
+def doc_reader2():
+    #Check for two arguments
+    if len(sys.argv) < 2:
+        print("You must have at least one document")
+        sys.exit()
+
+    #Create the parser object
+    parser = argparse.ArgumentParser(description="Create cat-style python application")
+
+    #Add the arguments
+    parser.add_argument("files", nargs="+", help="Grabs all files")
+
+    #create a newfile
+    newfile = open("new_document.txt", "w")
+
+    #Call parse_args function on the parser and assign to args variable
+    args = parser.parse_args()
+
+    #open and read each file from the command line
+    for arg in args.files:
+            with open(arg, encoding="utf-8") as arg:
+                read_data = arg.read()
+                #write the contents of each argv to the newfile
+                newfile.write(read_data)
+                newfile.write("\n")
+    arg.closed
+    newfile.close()
+
+doc_reader2()
