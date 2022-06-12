ae378ee1dd32d4eaf7d635965ebea2ef00ffb9f8
codekook <ralph.corey.1@gmail.com>
2022-06-12 07:23:28 -0700

    lmpthw: ex7 refactored

--- ex7.py
+++ ex7.py
@@ -8,6 +8,16 @@ def main():
         print("You must have at least one file and a regex pattern")
         sys.exit()
 
+    args = parse_args()
+    #read the file into memory
+    with open(args.files[0]) as file:
+        #iterate on the file object
+        for line in file:
+            #apply the regex pattern provided from command line look for a match
+            s = re.search(args.pattern[0], line)
+        print(s)
+
+def parse_args():
     #create an argparser to parse the command line arguments
     parser = argparse.ArgumentParser(description="Implement regex functions to match patterns using python")
 
@@ -18,17 +28,9 @@ def main():
     parser.add_argument("pattern", nargs=1, help="type regex pattern")
 
     #Call parse_args function on the parser and assign to args variable
-    args = parser.parse_args()
+    return parser.parse_args()
     #print(args.files[0])
     #print(args.pattern[0])
-
-    #read the file into memory
-    with open(args.files[0]) as file:
-        #iterate on the file object
-        for line in file:
-            #apply the regex pattern provided from command line look for a match
-            s = re.search(args.pattern[0], line)
-        print(s)
 
 if __name__ == "__main__":
     main()
